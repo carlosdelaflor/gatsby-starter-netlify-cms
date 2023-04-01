ee9785e8660a42648692aa2721686725a4329d65
Carlos de la Flor <carlitos.delaflor@gmail.com>
2023-04-01 13:22:38 -0500

    Tags screen updated.

--- src/pages/tags/index.js
+++ src/pages/tags/index.js
@@ -13,28 +13,32 @@ const TagsPage = ({
   },
 }) => (
   <Layout>
-    <section className="section">
-      <Helmet title={`Tags | ${title}`} />
-      <div className="container content">
-        <div className="columns">
-          <div
-            className="column is-10 is-offset-1"
-            style={{ marginBottom: "6rem" }}
-          >
-            <h1 className="title is-size-2 is-bold-light">Tags</h1>
-            <ul className="taglist">
-              {group.map((tag) => (
-                <li key={tag.fieldValue}>
-                  <Link to={`/tags/${kebabCase(tag.fieldValue)}/`}>
-                    {tag.fieldValue} ({tag.totalCount})
-                  </Link>
-                </li>
-              ))}
-            </ul>
+    <div className="main-content-container">
+      <section className="section">
+        <Helmet title={`Etiquetas | ${title}`} />
+        <div className="container content">
+          <div className="columns">
+            <div
+              className="column is-10 is-offset-1"
+              style={{ marginBottom: "6rem" }}
+            >
+              <h1 className="title is-size-2 is-bold-light">Etiquetas</h1>
+              <ul className="taglist">
+                {group.map((tag) => (
+                  <li key={tag.fieldValue}>
+                    <Link to={`/tags/${kebabCase(tag.fieldValue)}/`}>
+                      {tag.fieldValue} ({tag.totalCount})
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <section className="section">
+      </section>
+    </div>
   </Layout>
 );
 
